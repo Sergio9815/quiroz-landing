4ef5ae14be16d8db8a362986aef65a70e5d929f2
Sergio9815 <gsergio1598@gmail.com>
2021-10-28 02:42:35 -0500

    :art: initial configs

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,64 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <img class="nav__logo" src="./assets/logos/logo.png" alt="Quiroz-Govea Logo">
+      <section class="nav__links">
+        <router-link to="/">Inicio</router-link>
+        <router-link to="/Notaria">Notaría express</router-link>
+        <router-link to="/about">Sobre nosotros</router-link>
+        <router-link to="/Testimonios">Testimonios</router-link>
+        <router-link to="/Locations">Sucursales</router-link>
+        <router-link to="/Contact" @click.native="scrollToId('hola')">Contáctenos</router-link>
+      </section>
+      <section class="nav__social">
+        Social
+      </section>
     </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+    scrollToId(id) {
+      document.getElementById(id).scrollIntoView();
+    },
+  },
+};
+</script>
 <style>
+
+/* --- --- -- COLORS -- --- --- */
+:root {
+  --wine: #290111;
+  --brownLg: #A999A0;
+  --brown: #1700106b;
+  --brownDg: #170010;
+  --blue: #7FD4FF;
+  --gray: #c7c7c7;
+  --whiteLg: rgb(204, 204, 204);
+  --white: #fff;
+  --red: #F56A79;
+  --green: #71EFA3;
+}
+
+/* --- --- -- MAIN STYLES -- --- --- */
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  color: var(--white);
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -17,16 +67,50 @@
   color: #2c3e50;
 }
 
+/* --- --- -- LOCAL FONTS -- --- --- */
+@font-face {
+  font-family: 'Lato';
+  src: local('Lato'),
+    url('./assets/fonts/Lato-Regular.ttf') format('truetype');
+  font-weight: 400;
+}
+
+/* --- --- -- Menu BAR -- --- --- */
 #nav {
-  padding: 30px;
+  width: 100%;
+  height: 100px;
+  background-color: var(--wine);
+  font-family: Lato;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  font-weight: lighter;
+  color: var(--whiteLg);
+  text-decoration: none;
+}
+
+#nav a:hover {
+  color: var(--white);
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: var(--white);
+  border-bottom: 2px solid var(--white);
+}
+
+/* Logo de la empresa */
+.nav__logo {
+  width: 160px;
+  height: 110px;
+}
+
+.nav__links {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 780px;
 }
 </style>
